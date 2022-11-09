d6a6615858a09e34594ad8e9e7bd086484072a30
Ovi <sadmannk@gmail.com>
2022-11-09 14:46:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai';
-import { BsFillCartFill,BsFillSaveFill } from 'react-icons/bs';
-import {TbTruckDelivery} from 'react-icons/tb'
-import {FaUserFriends, FaWallet} from 'react-icons/fa'
-import {MdFavorite, MdHelp} from 'react-icons/md'
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HeadlineCards from './components/HeadlineCards';
 import Book from './components/Book';
